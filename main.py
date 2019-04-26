e5cd63f97617b9a5914b00c65a969f4f486a5ba0
Zach <zanussbaum@davidson.edu>
2019-04-26 11:51:10 -0400

    up to date

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from flask_socketio import SocketIO
 
 """
 TODO:
-    Fix html/JS/JQuery scripts so that you get form data correctly 
+    Be able to generate dynamic urls from homepage
+        figure out why teacher webpage is not rendering correctly 
+
+    Finish docstrings
 """
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,12 +16,33 @@ socketio = SocketIO(app)
 def homepace():
     return render_template('home.html')
     
-@app.route('/student')
-def student():
+@app.route('/<string:course>/<int:section>/<int:class_number>/student')
+def student(course, section, class_number):
+    """Webpage routine for a student in a certain class on a certain day 
+
+    Parameters:
+        course: a certain class number i.e. CSC351
+        section: section o fthe class
+        class_number: the current class day
+
+    Returns:
+        render_template('student.html'): renders the template for new session 
+    """
     return render_template('student.html')
 
-@app.route('/teacher')
-def teacher():
+@app.route('/teacher/<course>/<section>/<class_number>/<hash>')
+def teacher(course, section, class_number, hash):
+    """Webpage routine for a teacher in a certain class on a certain day 
+
+    Parameters:
+        course: a certain class number i.e. CSC351
+        section: section o fthe class
+        class_number: the current class day
+        hash: random hash generated for webpage
+
+    Returns:
+        render_template('teacher.html'): renders the template for new session
+    """
     return render_template('teacher.html')
 
 def messageReceived(methods=['GET', 'POST']):
@@ -35,6 +59,7 @@ def student_response(json, methods=['GET', 'POST']):
 @socketio.on('teacher question')
 def teacher_question(json, methods=['GET', 'POST']):
     print('a teacher posed a question')
+    print(json)
     socketio.emit('teacher question', json, callback=questionPosed)
 
 
