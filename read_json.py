99dc3da7cf0701316d62f26b2aaf3f631ec31746
Zach <zanussbaum@davidson.edu>
2019-05-07 14:57:59 -0400

    section loaded

--- read_json.py
+++ read_json.py
@@ -1,10 +1,20 @@
 import json
+from db import Sections, Teacher, Course_Section
 
 def read_json(file):
     with open("course-dump.json", 'r') as json_file:
-        array = json.load(json_file)
+        for line in json_file:
+            dict = json.loads(line)
+            subject = dict['Sub']
+            level = dict['Lvl']
+            section = dict['Sec']
+            teacher = dict['Instructor']
+            teacher = Teacher(data={'name': teacher, '_id':None})
+            course = Course_Section({'course':subject+level, 'section':section, '_id': None}, teacher)
 
-    print(array)
+            print("inserted course %s" %course)
+
+            Sections.insert_one(course)
 
 
 
