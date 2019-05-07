5e12c6cb2fea9ba94f24e51a0a6cc653ed97eec7
Conor Huh <cohuh@davidson.edu>
2019-05-07 03:26:32 -0400

    Added support for Database
    created brief documentation for logging entries into the database, retrieval methods for old sessions need to be filled out

---
+++ app.py
@@ -0,0 +1,300 @@
+import re
+import pprint
+from flask import Flask, render_template, redirect, url_for, request
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from random import getrandbits
+from db import Teachers, Teacher, Questions, Question, Sections, Course_Section, Course_Section_Meeting, Meetings
+
+"""
+Known Constraints:
+    Teacher must log in before student
+    Teacher must pose a question before any student submits a response
+    Student responses will only be logged to the most recent question posed
+
+"""
+
+"""
+TODO:
+    Create a way to input all the respones from a certain class --> database call, then put into {{}}
+        in html  
+
+    Make UI look better lol --> ask if this is necessary 
+
+    On teacher disconnect, the course meeting must be inserted into the our Meetings relation
+
+    Fix KeyError bugs, Disconnect Handler Errors, Message Handler Error
+"""
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+people = {}
+which_room = {}
+ids = {}
+questions = []
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        #may want to assert that prof name and student names are all lowercase to reduce duplicate entries into the db
+        teacher = request.form['teacherName']
+        course = request.form.get('course')
+        section = request.form.get('section')
+        person = request.form.get('person')
+        print(person)
+
+        #this is where we would make a database call to see class number
+        class_number = 1
+        """This logic is the only logic I am a bit unsure of, I will fix it in the morning. I confused
+        the table names, so I just have to double check and make sure that it still works, I believe
+        everything is fine
+
+        """
+        if person == 'teacher':
+            class_number = Sections.setClassNumber(teacher=teacher,course=course,section=section)
+            #if the class number is still one, it means that this is our first class and must be the first time for the teacher
+            whichTeacher : 'Teacher'
+            if class_number == 1:
+                #insert a new teacher
+                try:
+                    data = {'name': teacher, '_id': None}
+                    whichTeacher = Teacher(data=data)
+                    Teachers.insert(whichTeacher)
+                except Exception as ex:
+                    print (str(ex))
+
+            else:
+                #find the teacher
+                try:
+                    whichTeacherDict = Sections.returnTeacher(name=teacher)
+                    whichTeacher = Teachers.find_one(query = whichTeacherDict)
+                except Exception as ex:
+                    print (str(ex))
+
+            #data dictionary declarations for session insertion
+            tempDict = {}
+            tempDict['course'] = course
+            tempDict['section'] = section
+            tempDict['_id'] = None
+
+            #Creating a new Course_Section
+            try:
+                newClass : 'Course_Section'
+                newClass = Course_Section(data=tempDict, teacher=whichTeacher)
+            except Exception as ex:
+                print ("newClass exception")
+                print (str(ex))
+
+            #Insert the newClass into our Sections relation
+            try: 
+                Sections.insert_one(course_section=newClass)
+            except Exception as ex:
+                print("insertion problem")
+                print(str(ex))
+
+            #Set the current section id to be the session we just added
+            ids['section'] = newClass.getId()
+
+            #Create a new meeting by passing in the newClass we just created
+            try:
+                newMeeting : 'Course_Section_Meeting'
+                newMeeting = Course_Section_Meeting(course_section = newClass)
+            except Exception as ex:
+                print(str(ex))
+
+            #set the Session Number to be the class number
+            try:
+                newMeeting.setSessionNumber(number = class_number)
+            except Exception as ex:
+                print(str(ex))
+            try:
+                Meetings.insert_one(course_section_meeting=newMeeting)
+            except Exception as ex:
+                print(str(ex))
+            
+            ids['meeting'] = newMeeting.getId()
+
+        if person == 'teacher':
+            hash = getrandbits(128)
+            return redirect(url_for('teacher', teacher=teacher, course=course, section=section, class_number=class_number, hash=hash))
+        else:
+            return redirect(url_for('student', teacher=teacher, course=course, section=section, class_number=class_number))
+    
+    return render_template('home.html')
+    
+@app.route('/student/<teacher>/<course>/<section>/<class_number>')
+def student(teacher, course, section, class_number):
+    """Webpage routine for a student in a certain class on a certain day 
+
+    Parameters:
+        course: a certain class number i.e. CSC351
+        section: section of the class
+        class_number: the current class day
+
+    Returns:
+        render_template('student.html'): renders the template for new session 
+    """
+    return render_template('student.html')
+
+@app.route('/teacher/<teacher>/<course>/<section>/<class_number>/<hash>')
+def teacher(teacher,course, section, class_number, hash, methods=['GET', 'POST']):
+    """Webpage routine for a teacher in a certain class on a certain day 
+
+    Parameters:
+        course: a certain class number i.e. CSC351
+        section: section of the class
+        class_number: the current class day
+        hash: random hash generated for webpage
+
+    Returns:
+        render_template('teacher.html'): renders the template for new session
+    """
+    
+    return render_template('teacher.html')
+
+
+def strip_url(json, is_student):
+    if is_student:
+        url = json[json.find('student'):]
+    else:
+        url = json[json.find('teacher'):json.rfind('/')]
+
+    return url
+
+@socketio.on('student connect')
+def student_connection(json):
+    student = request.sid
+    # teacher, course, section  = handle_url(json['url'])
+    print("student connection to url %s" %(json['url']))
+
+    url = strip_url(json['url'], True)
+    url = re.sub('student', '', url)
+
+    join_room(url)
+
+    people[url].append(student)
+
+    which_room.update({student: url})
+
+    print("%d people in the class %s" %(len(people[url]), url))
+
+@socketio.on('teacher connect')
+def teacher_connection(json):
+    teacher = request.sid
+    # url = handle_url
+    print("teacher connection to url %s" %(json['url']))
+
+    url = strip_url(json['url'], False)
+    url = re.sub('teacher', '', url)
+
+    join_room(url)
+
+    people.update({url:[]})
+
+    which_room.update({teacher: url})
+
+    print("%d people in the class %s" %(len(people[url]), url))
+
+def messageReceived(methods=['GET', 'POST']):
+    print('student response was received!!!')
+
+def questionPosed(methods=['GET', 'POST']):
+    print('a teacher posed a question ')
+
+@socketio.on('student response')
+def student_response(json, methods=['GET', 'POST']):
+    #can parse json here to store into database
+    print('received a student response: ' + str(json))
+
+    current_Question : 'Question'
+    current_Question = Questions.getOneQuestion(query = ids)
+
+    if current_Question is not None:
+        try:
+            current_Question.addResponse(data = json)
+        except Exception as ex:
+            print("Question addition")
+            print(str(ex))
+    else:
+        print("Question has not been added")
+
+    room = which_room.get(request.sid)
+   
+
+    socketio.emit('student response', json, callback=messageReceived,room=room)
+
+
+@socketio.on('teacher question')
+def teacher_question(json, methods=['GET', 'POST']):
+    print('a teacher posed a question')
+
+    #can parse json here to put into database   
+    print(json)
+    
+    #Try to create the new question, passing in the question text
+    try:
+        newQuestion : 'Question'
+        newQuestion = Question(data = json)
+    except Exception as ex:
+        print("question formation")
+        print(str(ex))
+
+    #questions is simply a list to keep track of the number of questions we have, it is a flask work around
+    questions.append(1)
+
+    #set the question number for this session
+    newQuestion.setQuestionNumber(counter = len(questions))
+
+    """ Insert the finished question into the Questions relation
+        ids['question'] holds the last question previously posed by the professor
+        Assuming that a professor will only ask one question at a time, this allows us to 
+        get the question and then add a response to it
+    """
+    try:
+        Questions.insert_one(question=newQuestion)
+        
+        ids['question'] = newQuestion.getQuestionId()
+    except Exception as ex:
+        print("Question Insertion")
+        print(str(ex))
+    
+    #Get the current meeting
+    try:
+        currentMeeting : 'Course_Section_Meeting'
+        currentMeeting = Meetings.find_one(query = ids)
+    except Exception as ex:
+        print("find current meeting")
+        print(str(ex))
+
+    #Add the new Question to the current meeting relation
+    try:
+        currentMeeting.addQuestion(question = newQuestion)
+    except Exception as ex:
+        print("add question to meeting")
+        print(str(ex))
+
+    room = which_room.get(request.sid)
+
+    socketio.emit('teacher question', json, callback=questionPosed,room=room)
+
+
+
+@socketio.on('disconnect')
+def disconnect():
+    user = request.sid
+
+    room = which_room.get(user)
+
+    leave_room(room)
+
+    people[room].remove(user)
+
+    print("there are now %d users in %s" %(len(people[room]), room))
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
--- db.py
+++ db.py
@@ -11,19 +11,29 @@ def get_database() -> 'pymongo.database.Database':
     return MONGO_CLIENT.get_database(MONGO_DATABASE)
 
 class Teacher:
-    def __init__(self,data: str):
-        self.data_dict = data if type(data) is dict else json.loads(data)
-        self.name = self.data_dict.get("name")
-        self.id = None
+    def __init__(self, data: dict):
+        self.name = data['name']
+
+        if data['_id'] is None:
+            self.id = None
+        else:
+            self.id = data['_id']
 
     def getName(self):
         return self.name
+    
+    def getId(self):
+        return self.id
 
-class _Teachers:
+    def to_dict(self):
+        _return = dict(name = self.name)
+        return _return if self.id is None else _return.update({"_id": self.id})
+
+class _Teachers(Teacher):
     def __init__(self):
         self.db = get_database()
         self.collection = self.db["teachers"]
-    
+     
     #I believe we have to pass in all the information for a particular teacher here, not just the name
     def find_one(self, query: dict) -> 'Teacher':
         item = self.collection.find_one(query)
@@ -38,96 +48,197 @@ class _Teachers:
         return teacher
 
 class Question:
-    def __init__(self, data: str):
-        self.data_dict = data if type(data) is dict else json.loads(data)
-        self.question = self.data_dict.get("text")
-        self.map = {}
+    def __init__(self, data: dict):
+        self.question = data['question']
+        
+        self.responses = {}
+
+        self.number = None
+            
         self.id = None
 
-    def setResponse(self, student, respose):
-        if student not in self.map:
-            self.map[student] = response
+    def getQuestion(self):
+        return self.question
+    
+    def getQuestionNumber(self):
+        return self.number
+    
+    def getQuestionId(self):
+        return self.id
 
-class _Questions:
+    def getStudentResponses(self):
+        return self.responses
+
+    def setQuestionNumber(self, counter):
+        self.number = counter
+       
+
+    def addResponse(self, data: dict):
+        if data['user_name'] not in self.responses:
+            self.responses[data['user_name']] = data['message']
+            
+    def to_dict(self):
+        _return = dict(question=self.question, responses = self.responses, number = self.number)
+        # _return['responses'] = self.responses
+        return _return if self.id is None else _return.update({"_id": self.id})
+    
+
+class _Questions(Question):
     def __init__(self):
         self.db = get_database()
         self.collection = self.db["questions"]
 
     def getOneQuestion(self, query: dict) -> 'Question':
-        item = self.collection.find_one(query)
+        id = query['question']
+        item = self.collection.find_one({"_id" : id})
         return None if item is None else Question(item)
     
     def getAllQuestions(self, query: dict) -> 'list(Question)':
         return (Question(item) for item in self.collection.getAllQuestions(query))
 
     def insert_one(self, question: 'Question') -> 'bson.ObjectId':
-        obj_id = self.collection.insert_one(question.to_dict)
+        obj_id = self.collection.insert_one(question.to_dict())
         question.id = obj_id.inserted_id
         return question
 
 
+
 # Dict key 'teacher' here must be a teacher object 
 class Course_Section(Teacher):
-    def __init__(self, data: dict):
-        self.code = data['code']
-        self.professor_data = data['teacher']
+    def __init__(self, data: dict, teacher: 'Teacher'):
+        self.course = data['course']
         self.section = data['section']
-        self.department = data['department']
-        self.id = None
+        
+        #self.class_number = data['class_number']
+        
+        self.teacher = teacher
 
-    def returnCourseSectionDetails(self,socket):
-        temp = "Course Sections Details: " + self.code + " " + self.professor + " " + self.section + " " + self.department
-        socket.send(temp)
+        if data['_id'] is None:
+            self.id = None
+        else:
+            self.id = data['_id']
 
-class _Course_Sections:
-    def __init__(self, data: dict):
+    def returnCourseSectionDetails(self):
+        temp = "Course Sections Details: " + "\nProfessor: " + self.teacher.getName() + "\nCourse: " + self.course + "\nSection: " + self.section 
+        return temp
+
+    def getCourse(self):
+        return self.course
+    
+    def getSection(self):
+        return self.section
+
+    def getProfData(self):
+        return self.teacher
+
+    def getId(self):
+        return self.id
+
+    def to_dict(self):
+        _return = dict(course= self.course, section = self.section, teacher = self.teacher.to_dict())
+        return _return if self.id is None else _return.update({'_id': self.id})
+
+class _Course_Sections(Course_Section,Teacher):
+    def __init__(self):
         self.db = get_database()
         self.collection = self.db['course_sections']
 
     def insert_one(self, course_section : 'Course_Section') -> 'bson.ObjectId':
-        obj_id = self.collection.insert_one()
+        obj_id = self.collection.insert_one(course_section.to_dict())
         course_section.id = obj_id.inserted_id
     
     def getCourseByProfessor(self, teacher: 'Teacher') -> 'Course_Section':
-        for i in self.collection:
+        for i in self.collection.find():
             if i.professor_data.getName() == teacher.getName():
-                #Not sure which one to send here 'socket' or 'return'
                 return i
 
+    def setClassNumber(self, teacher, course, section):
+        class_number =0
+
+        for i in self.collection.find():
+            if course == i['course']:
+                if section == i['section']:
+                    if i['teacher'] is None:
+                        return class_number
+                    else:
+                        if teacher == i['teacher'].getName():
+                            class_number+=1
+
+        return class_number
+
+    def returnTeacher(self, name):
+        for i in self.collection.find():
+            if i['teacher'] is None:
+                return None
+            else:
+                return i['teacher'].getName()
+
 class Course_Section_Meeting(Course_Section, Question, Teacher):
-    def __init__(self, course_section: 'Course_Section', question = []):
-        self.session_number = None
-        self.meeting_map = {}
+    def __init__(self, course_section: 'Course_Section'):
+        self.questions = []
         self.date = datetime.datetime.now()
         self.id = None
-
+        self.session_number = 1
         self.course_section = course_section
 
-        #setting map to map from a date to a list of questions
+    def setSessionNumber(self, number):
+        self.session_number = number
+        
+    #adding questions to a question list
+    def addQuestion(self, question: 'Question'):
         if question is not None:
-            self.meeting_map.put(self.date, question)
-
+            self.questions.append(question)
+            
     def getSessionNumber(self):
-        return self.session
+        return self.session_number
     
     def getDate(self):
         return self.date
+    
+    def getId(self):
+        return self.id
 
-class _Course_Section_Meeting(Course_Section, Question, Teacher):
-    def __init__(self, data: dict):
+    def to_dict(self):
+        _return = dict(questions= self.questions, date = self.date, course_section = self.course_section.to_dict(), session_number = self.session_number)
+        return _return if self.id is None else _return.update({"_id" : self.id})
+
+class _Course_Section_Meetings(Course_Section, Question, Teacher):
+    def __init__(self):
         self.db = get_database()
         self.collection = self.db['course_section_meetings']
     
     def insert_one(self, course_section_meeting: 'Course_Section_Meeting'):
-        obj_id = self.collection.insert_one()
-        course_section_meeting.session_number = obj_id.inserted_id
+        obj_id = self.collection.insert_one(course_section_meeting.to_dict())
+        course_section_meeting.id = obj_id.inserted_id
         return course_section_meeting
 
+    def find_one(self, query:dict):
+        id = query['meeting']
+        item = self.collection.find_one({'_id': id})
+        return None if item is None else Course_Section_Meeting(item)
+
     def findByProfessor(self, teacher: 'Teacher'):
-        for i in self.collection:
+        for i in self.collection.find():
             if i.course_section.professor_data == teacher:
                 return i
+   
+    def getNumSessions(self, section: 'Course_Section'):
+        number = 0
+        for i in self.collection.find():
+            if section == i:
+                number += 1
+        return number
 
+    def output(self) -> 'list of meetings':
+        output = []
+        for i in self.collection.find():
+            output.append(i)
+        return output
+                
+Teachers = _Teachers()
+Questions = _Questions()
+Sections = _Course_Sections()
+Meetings = _Course_Section_Meetings()
 
 
 
